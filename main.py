a906cc08e5d031829201fd94a60175291841802b
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-11-04 06:07:19 +0100

    Remove api key

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 import os
 
-topic = "kosovo"
-url = "https://newsapi.org/v2/everything?q=kosovo&from=2023-10-28&to=from=2023-11-04&sortBy=publishedAt&apiKey=e768b4d8baf14731b5b8f124ee08bca6"
 api_key = os.getenv("PMCNewsAPIKey")
+topic = "kosovo"
 
+url = f"https://newsapi.org/v2/everything?\
+q={topic}\
+&from=2023-10-28\
+&to=from=2023-11-04\
+&sortBy=publishedAt\
+&apiKey={api_key}"
 
 response = requests.get(url)
 content = response.json()
